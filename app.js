ccd7235d019af233d6ec21d73c52a922304e50aa
Abdullah Sarwar <gmabdullah32@gmail.com>
2024-01-20 22:37:49 +0500

    add cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import "dotenv/config";
 import signupRoutes from "./routes/index.js";
 // import RegisterModel from "./model/index.js";
 import "./config/db.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8000; 
 
 app.set("view engine", "ejs");
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
