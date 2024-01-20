7e67a008bb42b965622fd1925bcb2f21782046a5
Abdullah Sarwar <gmabdullah32@gmail.com>
2024-01-20 22:46:52 +0500

    rename file to index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import "dotenv/config";
 import signupRoutes from "./routes/index.js";
-// import RegisterModel from "./model/index.js";
 import "./config/db.js";
 import cors from "cors";
 
@@ -17,20 +16,6 @@ app.use(express.json());
 app.use("/", (req, res) => {
     res.send("Welcom to blogs site");
 })
-
-// app.get("/register", (req, res) => {
-//     res.render("register")
-// });
-
-// app.post("/register_user", (req, res) => {
-//     const {firstname, lastname, email, password, cpassword } = req.body;
-
-//     const results = new RegisterModel({
-//         firstname, lastname, email, password, cpassword
-//     })
-//     // res.send(results).status(200);
-// })
-
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`))
 
